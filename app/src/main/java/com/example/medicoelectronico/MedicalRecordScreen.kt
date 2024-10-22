bed3ecc51dfb548ebf8797fd9cac630d6346a56c
BARBOSAOJ <pbarboje@myuax.com>
2024-10-22 17:53:56 +0200

    MedicalRecordViewModel.kt & MedicalRecordScreen.kt

--- app/src/main/java/com/example/medicoelectronico/MedicalRecordScreen.kt
+++ app/src/main/java/com/example/medicoelectronico/MedicalRecordScreen.kt
@@ -1,5 +1,17 @@
 package com.example.medicoelectronico
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
 @Composable
 fun MedicalRecordScreen(viewModel: MedicalRecordViewModel, userId: String) {
     val records by remember { mutableStateOf(listOf<MedicalRecord>()) }
