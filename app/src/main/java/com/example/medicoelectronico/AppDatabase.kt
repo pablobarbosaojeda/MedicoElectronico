7ed5023172fa04ce39494d1b52ba5f8432aa1b0c
BARBOSAOJ <pbarboje@myuax.com>
2024-11-10 09:53:36 +0100

    room database

---
+++ app/src/main/java/com/example/medicoelectronico/AppDatabase.kt
@@ -0,0 +1,28 @@
+package com.example.medicoelectronico
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [MedicalRecord::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun medicalRecordDao(): MedicalRecordDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "medical_history_db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
